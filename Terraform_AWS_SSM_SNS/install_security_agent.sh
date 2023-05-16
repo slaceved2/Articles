93a54a498ca3c314e616d422a28aec56ecf6983d
slaceved2 <130406858+slaceved2@users.noreply.github.com>
2023-05-15 19:24:17 -0500

    changed to raw url

--- Terraform_AWS_SSM_SNS/install_security_agent.sh
+++ Terraform_AWS_SSM_SNS/install_security_agent.sh
@@ -2,7 +2,7 @@
 # Security Agent Simulation - Install
 
 echo "Downloading security agent..."
-sudo wget -q https://github.com/slaceved2/Articles/blob/main/Terraform_AWS_SSM_SNS/security_agent --backups=0 -P /usr/bin
+sudo wget -q https://raw.githubusercontent.com/slaceved2/Articles/main/Terraform_AWS_SSM_SNS/security_agent --backups=0 -P /usr/bin
 echo "Security agent installation in progress..."
 sudo chmod +x /usr/bin/security_agent
 sleep 30
